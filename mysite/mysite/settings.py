196bf17870baf6cef794288deee333fed3c1699c
bronco91 <tept06@yahoo.com>
2017-08-25 16:22:06 -0300

    host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ra1554gh$kl0td6()-jn5$=#szk#lzcvfnq+$6&0^s2yeco^0('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bronco91.pythonanywhere.com/']
 
 
 # Application definition
